56d391a403441a1ff126d34f3c47f42046940352
khunzinshine <khunzinshine@ayainnovation.com>
2022-08-16 20:54:27 +0630

    [primitives, array, objects]

---
+++ basics.ts
@@ -0,0 +1,46 @@
+//Primitives: number, string, boolean
+let age: number;
+age = 24;
+
+let userName: string;
+userName = "Mg Mg";
+
+let isAdmin: boolean;
+isAdmin = true || false;
+
+let course: string | number = "React + Typescript";
+course = 96;
+
+//More complex types: array, objects
+let hobbies: string[];
+hobbies = ["coding", "music", "table tennis"];
+
+let customer: {
+  name: string;
+  age: number;
+  active: boolean;
+};
+customer = {
+  name: "Kyaw Kyaw",
+  age: 32,
+  active: true,
+};
+
+let people: {
+    name: string;
+    age: number;
+    active: boolean;
+}[];
+
+
+//Type Aliases
+type Person = {
+    name: string;
+    age: number;
+    active: boolean;
+}
+let root: Person;
+let admin: Person[];
+
+
+//Function types, parameters
